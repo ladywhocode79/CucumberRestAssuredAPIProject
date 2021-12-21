eddbc096f45ccf40e97dccb0eab7a49f49259465
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-12-21 06:35:08 +0530

    added code for generic stepdefinations for multiple values

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,6 +1,12 @@
 Feature: Application Login
   Scenario: Home page default login
-    Given User is on Net Banking landing page
-    When User login into application with username and password
+    Given User is on NetBanking landing page
+    When User login into application with username "sonal" and password "12345"
     Then Home page is populated
-    And Cards are displayed
+    And Cards displayed are "true"
+
+  Scenario: Home page default login
+    Given User is on NetBanking landing page
+    When User login into application with username "sonali" and password "123456"
+    Then Home page is populated
+    And Cards displayed are "false"
--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -38,22 +38,34 @@ public class stepDefination {
 
     @Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
-        throw new PendingException();
+        System.out.println("navigated to login page");
+
+
     }
 
-    @When("^User login into application with username and password$")
+   /* @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
-        throw new PendingException();
-    }
+        System.out.println("navigated to login page with username and password");
+
+    }*/
+   @When("^User login into application with username \"([^\"]*)\" and password \"([^\"]*)\"$")
+   public void user_login_into_application_with_username_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+       System.out.println("navigated to login page with username "+strArg1+" and password " + strArg2);
+   }
 
     @Then("^Home page is populated$")
     public void home_page_is_populated() throws Throwable {
-        throw new PendingException();
+        System.out.println("navigated to home page");
+
     }
 
-    @And("^Cards are displayed$")
+    /*@And("^Cards are displayed$")
     public void cards_are_displayed() throws Throwable {
-        throw new PendingException();
+        System.out.println("cards displayed");
+    }*/
+    @And("^Cards displayed are \"([^\"]*)\"$")
+    public void cards_displayed_are_something(String strArg1) throws Throwable {
+        System.out.println("cards displayed are "+strArg1);
     }
 
 }
